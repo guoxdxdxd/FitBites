f295737eb9f5bc852886cce831eb0fa494adf45c
xiongdi.guo <xiongdi.guo@qx.com>
2025-06-16 10:33:30 +0800

    feat(core): 丰富餐单状态枚举
    - 新增 Disabled、Generating 和 Fail 三个状态
    - 用于更精确地表示餐单生成和启用状态

--- src/FitBites.Core/Enums/MealPlanStatus.cs
+++ src/FitBites.Core/Enums/MealPlanStatus.cs
@@ -13,6 +13,22 @@ namespace FitBites.Core.Enums
         /// <summary>
         /// 启用
         /// </summary>
-        Active = 1
+        Active = 1,
+        
+        /// <summary>
+        /// 禁用
+        /// </summary>
+        Disabled = 4,
+        
+        /// <summary>
+        /// 生成中
+        /// </summary>
+        Generating = 2,
+        
+        /// <summary>
+        /// 生成失败
+        /// </summary>
+        Fail = 3
+        
     }
 } 
